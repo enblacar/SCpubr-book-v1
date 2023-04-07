053fe25f53f4acd441f3af4cbefffb411e64b96c
enblacar <enblacar1996@gmail.com>
2023-04-07 16:05:43 +0200

    Generated a duplicate of original repo to contain legacy v1 website.

---
+++ _common.R
@@ -0,0 +1,16 @@
+# example R options set globally
+options(width = 60)
+
+# example chunk options set globally
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  message = FALSE,
+  warning = FALSE,
+  fig.align = 'center',
+  fig.width = 6,
+  fig.height = 6,
+  out.width = "100%",
+  out.height = "100%",
+  dpi=300
+)
